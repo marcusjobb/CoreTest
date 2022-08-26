2aeeac57bd339de50eae53667519ac04fc0d9d5b
Marcus Medin <medina.jobb@outlook.com>
2022-08-26 11:32:51 +0200

    fixed

--- CoreTestTests/Controllers/CalcControllerTests.cs
+++ CoreTestTests/Controllers/CalcControllerTests.cs
@@ -14,7 +14,7 @@ namespace CoreTest.Controllers.Tests
             // Act
             var result = test.Add(1, 2);
             // Assert
-            Assert.AreEqual(4, result);
+            Assert.AreEqual(3, result);
         }
     }
 }
